7ba0d5ead641201306d5c8fe7a56ca600bcab872
lpotvin <ludopotvin@gmail.com>
2025-11-29 14:39:35 -0500

    finish

--- devoir3_etudiants.cpp
+++ devoir3_etudiants.cpp
@@ -5,6 +5,7 @@
 //CE FICHIER N'EST PAS EXHAUSTIF, IL DONNE SIMPLEMENT DES EXEMPLES ET UNE CERTAINE STRUCTURE.À Vous de modifier.
 ////////////////////////////////////////////////////////////////
 
+#include <climits>
 #include <iostream>
 #include <memory>
 #include <stdlib.h>
@@ -17,6 +18,7 @@
 
 #define PAGE_t 256 //Taille d'une page (256 bytes)
 #define TAILLE_MEMOIRE_PHYSIQUE 256 * PAGE_t
+#define TLB_SIZE 16
 
 ////////////////////////////////////////////////////////////////
 //Cette fonction retourne la valeur du byte signé
@@ -66,6 +68,7 @@ void affichageResultats(
     const int adressePhysique[], 
     const char memPhysique[],
     int pageFaultCount,
+    int TLBCount,
     int totalAdresses
 ) 
 {
@@ -81,7 +84,9 @@ void affichageResultats(
     }
 
     // Stats
-    std::cout << "Page fault %: " << ( (double)pageFaultCount / totalAdresses ) * 100 << "\n";
+    std::cout << "=== Stats === \n";
+    std::cout << "Page fault: " << ( (double)pageFaultCount / totalAdresses ) * 100 << "%\n";
+    std::cout << "TLB hit: " << ( (double)TLBCount / totalAdresses ) * 100 << "%\n";
 }
 
 int chargerPageDansFrame(int page, int frame, char memPhysique[TAILLE_MEMOIRE_PHYSIQUE]){
@@ -93,6 +98,74 @@ int chargerPageDansFrame(int page, int frame, char memPhysique[TAILLE_MEMOIRE_PH
     fichier.close();
 
     return physical_addresse;
+}
+
+// TLB
+struct TLBEntry {
+    int page;
+    int frame;
+    int lastUse = -1;
+};
+
+int findMaxUseTLB(TLBEntry TLB[TLB_SIZE])
+{
+  int max = -1;
+  for(int i = 0; i < TLB_SIZE; i++)
+  {
+    if(TLB[i].lastUse > max)
+    {
+      max = TLB[i].lastUse;
+    }
+  }
+  return max;
+}
+
+int findMinUseTLB(TLBEntry TLB[TLB_SIZE])
+{
+  int min = INT_MAX;
+  for(int i = 0; i < TLB_SIZE; i++)
+  {
+    if(TLB[i].lastUse < min)
+    {
+      min = TLB[i].lastUse;
+    }
+  }
+  return min;
+}
+
+int findFrameTLB(TLBEntry TLB[TLB_SIZE], int page)
+{
+  int max = findMaxUseTLB(TLB);
+  int frame = -1;
+
+  for(int i = 0; i < TLB_SIZE; i++)
+  {
+    if(TLB[i].page == page)
+    {
+      TLB[i].lastUse = max + 1;
+      frame = TLB[i].frame;
+      std::cout << "TLB hit: " << page << " est dans TLB: " << frame << std::endl;
+      break;
+    }
+  }
+  return frame;
+}
+
+void addToTLB(TLBEntry TLB[TLB_SIZE], int page, int frame)
+{
+
+  int max = findMaxUseTLB(TLB);
+  int min = findMinUseTLB(TLB);
+
+  for(int i = 0; i < TLB_SIZE; i++)
+  {
+    if(TLB[i].lastUse == min)
+    {
+      int lastUse = max + 1;
+      TLB[i] = {page, frame, lastUse};
+      break;
+    }
+  }
 }
 
 //////////////////////
@@ -111,7 +184,7 @@ int main()
 	int adressePhysique[1000] = {0};
 	int tablePage[256][2]={0};
 	std::vector<int>adresseLogique;
-  int TLB[16] = {};
+  TLBEntry TLB[TLB_SIZE] = {};
 	
 	// 2. Lire le fichier d'adresses à traduire
   std::ifstream fichier("addresses.txt");
@@ -141,15 +214,21 @@ int main()
   // 4. Charger les adresses physiques
   int indexFrame = 0;
   int pageFaultCount = 0;
+  int TLBCount = 0;
 
 	for(int i=0;i<bits_page.size();i++)
 	{
     int page = bits_page[i];
     int offset = bits_offset[i];
-    int frame = -1;
 
-    // TODO Validate TLB
+    // 4.1 validation TLB
+    int frame = findFrameTLB(TLB, page);
+    if (frame != -1)
+    {
+      TLBCount++;
+    }
 
+    // 4.2 Memoire Physique
     if (frame == -1)
     {
       if(tablePage[page][1] != 1)
@@ -165,7 +244,9 @@ int main()
         indexFrame++;
       }
 
+      // Deja en memoire, recupere le frame et met a jour le TLB
       frame = tablePage[page][0];
+      addToTLB(TLB, page, frame);
     }
 
     adressePhysique[i] = frame + offset;
@@ -173,7 +254,7 @@ int main()
 
 	// 5. Ecrire le fichier de sortie
   int total_adresse = bits_page.size();
-  affichageResultats(adresseLogique, adressePhysique, memPhysique, pageFaultCount, total_adresse);
+  affichageResultats(adresseLogique, adressePhysique, memPhysique, pageFaultCount, TLBCount, total_adresse);
 
 	return 0;
 }
